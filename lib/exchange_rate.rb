e2cad27b30b1f32c84458883b4f7cef02cd41cfa
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-16 17:36:39 +0100

    Handle incorrect date format

--- lib/exchange_rate.rb
+++ lib/exchange_rate.rb
@@ -1,13 +1,17 @@
 require "exchange_rate/version"
 require "exchange_rate/data_loader"
 require "exchange_rate/base"
+require "bigdecimal"
 
 module ExchangeRate
   def at(date, base, other)
+    date = date_string(date_rep)
     ex_rates = Base.new
-    base_rate = ex_rates.rates_at(date.to_s, base)
-    other_rate = ex_rates.rates_at(date.to_s, other)
-    other_rate / base_rate
+    base_rate = ex_rates.rates_at(date, base)
+    base = BigDecimal(base_rate.to_s)
+    other = ex_rates.rates_at(date, other)
+    other_rate = BigDecimal(other.to_s)
+    other_rate / base
   end
 
   def available_currencies
@@ -28,9 +32,29 @@ module ExchangeRate
     DataLoader.save_to_disk
   end
 
+  def valid_dd_mm_yyyy(date_string)
+    matches = date_string =~ /^(3[0-1]|0[1-9]|[1-2][0-9])(?:(1[0-2]|0[1-9])|/(1[0-2]|0[1-9])/)([0-9]{4})/
+    raise ArgumentError, 'Not a valid date string (dd/mm/yyyy)' unless matches
+    date_string
+  end
+
+  def date_string(date_rep)
+    if date_rep.is_a? Date
+      date_rep.to_s
+    elsif date_rep.is_a? String
+      valid_dd_mm_yyyy(date_rep)
+    else
+      raise ArgumentError, "'date' must be a Date or 'dd/mm/yyyy' string"
+    end
+  end
+
   module_function :at,
                   :fetch_data_and_save_to_disk,
                   :available_currencies,
                   :available_dates,
-                  :most_recent
+                  :most_recent,
+                  :valid_dd_mm_yyyy,
+                  :date_string
 end
+
+
